98d799274ee41b2e3bdef75d2f220320b7960aa1
PerryP1 <perryapublico@gmail.com>
2018-07-12 14:15:11 -0400

    first commit

---
+++ src/RandomnessDebuggingActivity.java
@@ -0,0 +1,46 @@
+import java.util.Random;
+
+public class RandomnessDebuggingActivity {
+    public static void main(String[] args) {
+        output("Generate 10 random integers between 1 and 6");
+        Random rand = new Random();
+
+        for (int i = 1; i <= 10; ++i) {
+            int randomInt = 1 + rand.nextInt(6);
+            output("Generated number: " + randomInt);
+        }
+
+        output("Done.");
+    }
+
+    private static void output(String aMessage) {
+        System.out.println(aMessage);
+    }
+}
+
+
+//This program should generate 10 random integers from 1 to 6. Fix it so that it works...
+//
+//import java.util.random;
+//
+//public class RandomGenerator{
+//    public static void main(String[] args)
+//    {
+//        output("Generate 10 random integers between 0 and 6");
+//        Random rnd;
+//
+//        for (int i = 1; i <= 12; ++i)
+//        {
+//          int randomInt = 1 + rnd.nextInt(0 to 6);
+//          output("Generated number: " + randomInt);
+//        }
+//
+//        output("Done.");
+//    }
+//
+//  private static void output(String aMessage)
+//  {
+//    System.out.println(aMessage);
+//  }
+//}
+//*
